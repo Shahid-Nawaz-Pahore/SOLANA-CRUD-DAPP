2d7c982f90b39382fe2354167a0aea2034580859
Shahid Nawaz Blockchain Engineer <shahidnawaz.edu.pk@gmail.com>
2025-03-05 01:20:17 +0500

    create accounts

--- CRUD/anchor/programs/CRUD/src/lib.rs
+++ CRUD/anchor/programs/CRUD/src/lib.rs
@@ -7,7 +7,32 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 #[program]
 pub mod CRUD {
     use super::*;
-
+  
 
 }
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct CreateEntry<'info> {
+  #[account(
+    init,
+    seeds = [title.as_bytes(), owner.key().as_ref()],
+    bump,
+    space = 8 + JournalEntryState::INIT_SPACE,
+    payer = owner,
+  )]
+  pub journal_entry: Account<'info, JournalEntryState>,
+  #[account(mut)]
+  pub owner: AccountInfo<'info>,
+  pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct JournalEntryState {
+  pub owner: Pubkey,
+  #[max_len(50)]
+  pub title: String,
+  #[max_len(1000)]
+  pub message: String,
+}
