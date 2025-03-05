57a59bf148faa0cd562c79f556ef075d8f6cdea4
Shahid Nawaz Blockchain Engineer <shahidnawaz.edu.pk@gmail.com>
2025-03-05 07:39:46 +0500

    create is done

--- CRUD/anchor/programs/CRUD/src/lib.rs
+++ CRUD/anchor/programs/CRUD/src/lib.rs
@@ -7,6 +7,14 @@ declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 #[program]
 pub mod CRUD {
     use super::*;
+
+    fn create_journal_entry(ctx: Context<CreateEntry>, title: String, message:String) ->Result<()> {
+      let journal_entry  = &mut ctx.accounts.journal_entry;
+      journal_entry.owner = *ctx.accounts.owner.key;
+      journal_entry.title = title;
+      journal_entry.message = message;
+      Ok(())
+    }
   
 
 }
